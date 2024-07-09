d4beeeadf273ca5354dd64dbf4bc88418e70f667
augustofaggionliradossantos <a.faggion.santos@gmail.com>
2024-07-09 23:37:45 +0200

    section about finished css

--- src/components/About.js
+++ src/components/About.js
@@ -1,40 +1,35 @@
 import howImg from "../img/how/kitchen.jpg";
+
 export default function About() {
   return (
     <div className="container-fluid py-5">
       <div className="row padding">
         <div class="col-md-6 left-side">
-        <h2 className="header-secondary">Über unser Unternehmen</h2>
-        <p className="text">
-          Bauenneun ist ein führendes Bauunternehmen, das sich auf die
-          Errichtung moderner und stilvoller Wohngebäude spezialisiert hat. Mit
-          jahrzehntelanger Erfahrung in der Baubranche haben wir uns einen Ruf
-          für Qualität, Zuverlässigkeit und außergewöhnlichen Kundenservice
-          erarbeitet. Unsere Mission ist es, unseren Kunden hochwertige,
-          maßgeschneiderte Häuser zu bieten, die ihre individuellen Bedürfnisse
-          und Wünsche widerspiegeln.
-        </p>
-        <p className="text">
-          Unser Team aus erfahrenen Architekten, Ingenieuren und Bauarbeitern
-          arbeitet eng mit jedem Kunden zusammen, um sicherzustellen, dass jedes
-          Projekt von der Planung bis zur Fertigstellung reibungslos verläuft.
-          Wir nutzen die neuesten Technologien und Baumethoden, um effiziente
-          und nachhaltige Bauwerke zu schaffen.
-        </p>
-        <p className="text">
-          Bei Bauenneun legen wir großen Wert auf Transparenz und
-          Kundenzufriedenheit. Deshalb bieten wir detaillierte 3D-Designs an,
-          damit unsere Kunden eine klare Vorstellung davon haben, wie ihr
-          zukünftiges Zuhause aussehen wird. Unser Ziel ist es, den Bauprozess
-          so stressfrei wie möglich zu gestalten und die Erwartungen unserer
-          Kunden zu übertreffen.
-        </p>
+          <h2 className="header-secondary">Über unser Unternehmen</h2>
+          <p className="text-about">
+            Bauenneun ist ein führendes Bauunternehmen, das sich auf moderne und
+            stilvolle Wohngebäude spezialisiert hat. Mit jahrelanger Erfahrung
+            bieten wir Qualität, Zuverlässigkeit und außergewöhnlichen
+            Kundenservice. Unsere Mission ist es, hochwertige, maßgeschneiderte
+            Häuser zu liefern, die Ihren Bedürfnissen entsprechen.
+          </p>
+          <p className="text-about">
+            Unser erfahrenes Team arbeitet eng mit jedem Kunden zusammen, um
+            sicherzustellen, dass jedes Projekt reibungslos verläuft. Wir nutzen
+            moderne Technologien und Baumethoden für effiziente und nachhaltige
+            Bauwerke.
+          </p>
+          <p className="text-about">
+            Transparenz und Kundenzufriedenheit stehen bei uns im Mittelpunkt.
+            Wir bieten detaillierte 3D-Designs an, damit unsere Kunden eine
+            klare Vorstellung ihres zukünftigen Zuhauses haben. Unser Ziel ist
+            es, den Bauprozess stressfrei zu gestalten und die Erwartungen
+            unserer Kunden zu übertreffen.
+          </p>
         </div>
         <div className="col-md-6 right-side">
-
-        <img src={howImg} alt="kitchen" className="img-home" />
+          <img src={howImg} alt="kitchen" className="img-home" />
         </div>
-
       </div>
     </div>
   );
