45cf76ad3ce2b41b0cc181f19de5f0fab2cc09ff
augustofaggionliradossantos <a.faggion.santos@gmail.com>
2024-07-09 23:30:12 +0200

    how it works section done

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -21,27 +21,26 @@ export default function HowItWorks() {
       </div>
       <div class="col-md-6">
         <div>
-          <h2 className="header-secondary">Wie Gebäude funktionieren</h2>
-          <p className="text">
+          <h2 className="header-secondary-how">Wie Gebäude funktionieren</h2>
+          <p className="text-info">
             In dieser Phase können Sie das Aussehen und das Gefühl Ihres Hauses
             in 3D gestalten, mit detaillierten Spezifikationen, damit das
             Endprodukt so nah wie möglich an Ihrer Vision ist.
           </p>
-          <button className="btn">Fragebogen ausfüllen</button>
         </div>
         <div>
-          <h2 className="header-secondary">Was Sie bekommen...</h2>
-          <p className="text">
+          <h2 className="header-secondary-how">Was Sie bekommen...</h2>
+          <p className="text-info">
             Ihr Zuhause wird mindestens 20% weniger kosten als bei unseren
             Mitbewerbern, was bedeutet, dass Sie mindestens 200.000 $ bei einem
             92,9 m² großen Haus sparen, wenn Sie mit uns bauen.
           </p>
-          <p className="text">
+          <p className="text-info">
             Ihr Zuhause wird spätestens 1 Monat nach dem geplanten
             Fertigstellungstermin fertiggestellt, solange keine
             unvorhergesehenen Umstände auftreten.
           </p>
-          <p className="text">
+          <p className="text-info">
             Sie sind so gut wie garantiert, mit dem Haus, das Sie erhalten,
             zufrieden zu sein, weil wir genau das entwerfen, was Sie in 3D
             gebaut haben.
